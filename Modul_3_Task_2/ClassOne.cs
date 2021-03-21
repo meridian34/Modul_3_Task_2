eb251d232fcf829d621c537ada78813f76bb7317
meridian34 <sobol.andrey.25@gmail.com>
2021-03-21 21:21:51 +0200

    описан класс 1

---
+++ Modul_3_Task_2/ClassOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul_3_Task_2
+{
+    class ClassOne
+    {
+        public Action<bool> ShowHandler;
+        public int Pow(int x, int y) => x * y;
+    }
+}
