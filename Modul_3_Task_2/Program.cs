ede17f6540f990dcadcd2c08dbe30c671e61365a
meridian34 <sobol.andrey.25@gmail.com>
2021-03-21 21:23:59 +0200

    Модифицирован класс Program

--- Modul_3_Task_2/Program.cs
+++ Modul_3_Task_2/Program.cs
@@ -6,7 +6,16 @@ namespace Modul_3_Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var one = new ClassOne();
+            var two = new ClassTwo();
+            one.ShowHandler = new Program().Show;
+            one.ShowHandler(two.Calc(one.Pow, 1, 2).Invoke(2));
+            Console.ReadKey();
+        }
+
+        public void Show(bool result)
+        {
+            Console.WriteLine(result);
         }
     }
 }
