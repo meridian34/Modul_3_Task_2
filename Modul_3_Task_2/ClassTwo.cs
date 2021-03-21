2bd20913f8eb2793e539bf8df5c3899d7c29b6dd
meridian34 <sobol.andrey.25@gmail.com>
2021-03-21 21:23:01 +0200

    описан класс 2

---
+++ Modul_3_Task_2/ClassTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul_3_Task_2
+{
+    class ClassTwo
+    {
+        private int _result;
+        public ClassTwo()
+        {
+            _result = 0;
+        }
+
+        public Func<int, bool> Calc(Func<int, int, int> PowHandler, int x, int y)
+        {
+            _result = PowHandler.Invoke(x, y);
+
+            return (x) => Result(x);
+        }
+
+        public bool Result(int x)
+        {
+            return _result % x == 0 ? true : false;
+        }
+    }
+}
